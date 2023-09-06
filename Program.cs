a1801cba8d837543d1c9f89de454efbfa780371d
halvarsson <halvarsson@gmail.com>
2023-09-06 17:04:53 +0200

    add test rtmp stream

--- Program.cs
+++ Program.cs
@@ -1,7 +1,71 @@
-﻿using static DirectXDemoConsoleApp.Window;
+﻿using RtspClientSharp;
+using RtspClientSharp.Rtsp;
+using static DirectXDemoConsoleApp.Window;
 GameOverlay.TimerService.EnableHighPrecisionTimers();
+
+
+var serverUri = new Uri("rtmp://localhost/mystream");
+
+var connectionParameters = new ConnectionParameters(serverUri);
+var cancellationTokenSource = new CancellationTokenSource();
+
+Task connectTask = ConnectAsync(connectionParameters, cancellationTokenSource.Token);
+
 
 using (var example = new Example())
 {
     example.Run();
 }
+
+    static async Task ConnectAsync(ConnectionParameters connectionParameters, CancellationToken token)
+    {
+        try
+        {
+            TimeSpan delay = TimeSpan.FromSeconds(5);
+
+            using (var rtspClient = new RtspClient(connectionParameters))
+            {
+                rtspClient.FrameReceived +=
+                    (sender, frame) => Console.WriteLine($"New frame {frame.Timestamp}: {frame.GetType().Name}");
+
+                while (true)
+                {
+                    Console.WriteLine("Connecting...");
+
+                    try
+                    {
+                        await rtspClient.ConnectAsync(token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        return;
+                    }
+                    catch (RtspClientException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        await Task.Delay(delay, token);
+                        continue;
+                    }
+
+                    Console.WriteLine("Connected.");
+
+                    try
+                    {
+                        await rtspClient.ReceiveAsync(token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        return;
+                    }
+                    catch (RtspClientException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        await Task.Delay(delay, token);
+                    }
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+        }
+    }
